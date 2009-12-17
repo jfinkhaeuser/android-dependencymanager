2b5c714b1195a28da6b5e2caf5b88c2e370feb68
unwesen <unwesen@a96de4bc-ea45-11de-88ac-396b0f65d9f6>
2009-12-17 17:52:21 +0000

    Add convenience functions for parsing/serializing lists of Intents

--- trunk/common/src/de/finkhaeuser/dm/common/Intents.java
+++ trunk/common/src/de/finkhaeuser/dm/common/Intents.java
@@ -19,9 +19,12 @@
 
 package de.finkhaeuser.dm.common;
 
+import android.net.Uri;
+
 import android.content.Intent;
 
-import android.net.Uri;
+import java.util.List;
+import java.util.LinkedList;
 
 
 /**
@@ -34,4 +37,67 @@ public class Intents
    **/
   // Action for installing packages
   public static final String ACTION_PACKAGE_INSTALL = "de.finkhaeuser.dm.intent.action.PACKAGE_INSTALL";
+
+
+
+  /***************************************************************************
+   * Convenience functions
+   **/
+
+  /**
+   * Parses serialized Intents from the query string of the given URI, assuming
+   * the parameter key of DepedencyManagerContract.QUERY_PARAM_INTENT. Returns
+   * null if no intents are found in the Uri.
+   **/
+  public static List<Intent> parseIntents(Uri uri)
+  {
+    List<String> values = uri.getQueryParameters(
+        DependencyManagerContract.QUERY_PARAM_INTENT);
+    if (null == values) {
+      return null;
+    }
+
+    LinkedList<Intent> results = new LinkedList<Intent>();
+    for (String v : values) {
+      try {
+        results.add(Intent.parseUri(v, Intent.URI_INTENT_SCHEME));
+      } catch (java.net.URISyntaxException ex) {
+        // pass
+      }
+    }
+
+    if (0 >= results.size()) {
+      return null;
+    }
+
+    return results;
+  }
+
+
+
+  /**
+   * Serializes the list of Intents into a query string format, using
+   * DependencyManagerContract.QUERY_PARAM_INTENT as the parameter key.
+   * Returns null if the list is null or empty.
+   **/
+  public static String serializeIntents(List<Intent> intents)
+  {
+    if (null == intents || 0 >= intents.size()) {
+      return null;
+    }
+
+    String result = new String();
+    for (Intent i : intents) {
+      result = String.format("%s&%s=%s", result,
+          DependencyManagerContract.QUERY_PARAM_INTENT,
+          i.toUri(Intent.URI_INTENT_SCHEME));
+    }
+
+    if (0 >= result.length()) {
+      return null;
+    }
+
+    // Trim leading ampersand
+    return result.substring(1);
+  }
 }
