02591c2c8750c7fc4e120eed6d546770468e5f58
unwesen <unwesen@a96de4bc-ea45-11de-88ac-396b0f65d9f6>
2009-12-23 12:19:49 +0000

    * Add build files for client lib * Add wrapper around IDependencyManager

--- trunk/client/src/de/finkhaeuser/dm/client/DependencyManager.java
+++ trunk/client/src/de/finkhaeuser/dm/client/DependencyManager.java
@@ -19,6 +19,94 @@
 
 package de.finkhaeuser.dm.client;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.ComponentName;
+
+import android.os.IBinder;
+import android.os.RemoteException;
+
+import de.finkhaeuser.dm.IDependencyManager;
+
+import android.util.Log;
+
+/**
+ * Client object to the IDependencyManager interface; encapsulates binding to
+ * the service.
+ **/
 public class DependencyManager
 {
+  /***************************************************************************
+   * Private constants
+   **/
+  private static final String LTAG = "AggregateCursor";
+
+
+  /***************************************************************************
+   * Private data
+   **/
+  private Context             mContext;
+
+  // Service stub & lock for synchronous access
+  private Object              mStubLock = new Object();
+  private IDependencyManager  mStub = null;
+
+  // Service connection
+  private ServiceConnection   mConnection = new ServiceConnection() {
+    public void onServiceConnected(ComponentName className, IBinder service)
+    {
+      synchronized (mStubLock) {
+        mStub = IDependencyManager.Stub.asInterface(service);
+      }
+    }
+
+
+    public void onServiceDisconnected(ComponentName className)
+    {
+      synchronized (mStubLock) {
+        mStub = null;
+      }
+    }
+  };
+
+
+
+  /***************************************************************************
+   * Extra functions
+   **/
+  public DependencyManager(Context context)
+  {
+    mContext = context;
+  }
+
+
+
+  /***************************************************************************
+   * Implementation of IDependencyManager API
+   **/
+  public void resolveDependencies(String packageName)
+  {
+    // FIXME sort out synchronization
+
+    bindService();
+    try {
+      mStub.resolveDependencies(packageName);
+    } catch (RemoteException ex) {
+      Log.e(LTAG, "Exception " + ex.getMessage());
+    }
+  }
+
+
+
+  /***************************************************************************
+   * Private functions
+   **/
+  private void bindService()
+  {
+    // FIXME sort out synchronization
+
+    mContext.bindService(new Intent(IDependencyManager.class.getName()),
+        mConnection, Context.BIND_AUTO_CREATE);
+  }
 }
